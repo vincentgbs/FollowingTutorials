918c4d2e33561fb7785bf4f534c21efe1d41e6a1
vincent <vincent.gbs11@gmail.com>
2021-02-19 12:10:01 -0500

    Adding a line

--- bash/github.sh
+++ bash/github.sh
@@ -1,5 +1,17 @@
-# add a line to the test file
-echo "adding a line" >> log.txt
+#!/bin/bash
+### Mac script
 
-# remove the last line from the test file
-sed -i '' -e '$ d' log.txt
+COUNTER=20
+until [  $COUNTER -lt 10 ]; do
+    # add a line to the test file
+    echo "adding a line" >> log.txt
+    git add .
+    git commit -m "Adding a line"
+
+    # remove the last line from the test file
+    sed -i '' -e '$ d' log.txt
+    git add .
+    git commit -m "Removing a line"
+
+    let COUNTER-=1
+done
