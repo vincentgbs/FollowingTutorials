ec133a25a76132bd30be600ae9bef2fef8ad3940
vincent <vincent.gbs11@gmail.com>
2021-03-11 14:45:48 -0500

    Handling POST Requests lesson

--- coursera/CreateYourFirstWebAppwithPythonandFlask/routes.py
+++ coursera/CreateYourFirstWebAppwithPythonandFlask/routes.py
@@ -8,7 +8,10 @@ import forms
 def index():
     return render_template('index.html', current_title='Custom Title'
 
-@app.route('/about')
+@app.route('/about', methods=['GET', 'POST'])
 def about():
     form = forms.AddTaskForm()
+    if form.validate_on_submit():
+        print('Submitted title', form.title.data)
+        return render_template('about.html', form=form, title=form.title.data)
     return render_template('about.html', form=form)
