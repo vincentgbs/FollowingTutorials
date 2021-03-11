f5c24b1525f6a33b09b2f9b2366e85d41a47b680
vincent <vincent.gbs11@gmail.com>
2021-03-11 16:12:10 -0500

    2 Importing and Plotting Data Lesson

--- coursera/UnderstandingDeepfakeswithKeras/Learner_Notebook.py
+++ coursera/UnderstandingDeepfakeswithKeras/Learner_Notebook.py
@@ -13,3 +13,14 @@ from tensorflow.keras.models import Model, Sequential
 from PIL import Image
 
 print('TensorFlow version:', tf.__version__)"
+
+
+
+"(x_train, y_train), (x_test, y_test) = tfutils.datasets.mnist.load_data(one_hot=False)
+x_train = tfutils.datasets.mnist.load_subset([0], x_train, y_train)
+x_test = tfutils.datasets.mnist.load_subset([0], x_test, y_test)
+x = np.concatenate([x_train, x_test], axis=0)
+
+
+
+tfutils.datasets.mnist.plot_ten_random_examples(plt, x, np.zeros((x.shape[0], 1))).show()
