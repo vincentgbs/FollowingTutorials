15c9e29c12d48159faee303db30bafa730b7f7e3
vincent <vincent.gbs11@gmail.com>
2021-03-11 11:25:48 -0500

    Extending HTML Templates lesson

--- coursera/CreateYourFirstWebAppwithPythonandFlask/app.py
+++ coursera/CreateYourFirstWebAppwithPythonandFlask/app.py
@@ -8,5 +8,9 @@ app = Flash(__name__)
 def index():
     return render_template('index.html', current_title='Custom Title'
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
