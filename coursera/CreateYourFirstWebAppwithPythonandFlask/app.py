10f64c19d8604c68df60d83a93356c69fde2a8c3
vincent <vincent.gbs11@gmail.com>
2021-03-11 15:03:14 -0500

    SQLAlchemy lesson

--- coursera/CreateYourFirstWebAppwithPythonandFlask/app.py
+++ coursera/CreateYourFirstWebAppwithPythonandFlask/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flash(__name__)
 app.config['SECRET_KEY'] = 'random_string'
+ap.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+db = SQLAlchemy(app)dels.property
 
 # import routes AFTER instantiating app
 from routes import *
