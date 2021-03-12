2534340b68deeca0e1dd2e81de3991586ddf1696
vincent <vincent.gbs11@gmail.com>
2021-03-12 16:45:51 -0500

    4 Generator lesson

--- coursera/UnderstandingDeepfakeswithKeras/Learner_Notebook.py
+++ coursera/UnderstandingDeepfakeswithKeras/Learner_Notebook.py
@@ -28,7 +28,7 @@ tfutils.datasets.mnist.plot_ten_random_examples(plt, x, np.zeros((x.shape[0], 1)
 
 
 discriminator = Sequential([
-    Conv2D(64, 3, strides=2, input_shape(28, 28, 1)),
+    Conv2D(64, 3, strides=2, input_shape=(28, 28, 1)),
     LeakyReLU(),
     BatchNormalization(),
 
@@ -47,4 +47,26 @@ discriminator = Sequential([
 opt = tf.keras.optimizers.Adam(lr=2e-4, beta_1=0.5)
 
 discriminator.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
-discriminator.summary()
+discriminator.summary()"
+
+
+
+generator = Sequential([
+    Dense(256, activation='relu', input_shape=(noise_dim,)),
+    Reshape((1, 1, 256)),
+
+    Conv2DTranspose(256, 5, activation='relu'),
+    BatchNormalization(),
+    Conv2DTranspose(128, 5, activation='relu'),
+    BatchNormalization(),
+
+    Conv2DTranspose(64, 5, strides=2, activation='relu'),
+    BatchNormalization(),
+    Conv2DTranspose(32, 5, activation='relu'),
+    BatchNormalization(),
+
+    Conv2DTranspose(1, 4, activation='sigmoid')
+
+])
+
+generator.summary()
