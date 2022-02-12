b42688c4bde8165fa2d56725e269bac17f9f7cc1
vincent <vincent.gbs11@gmail.com>
2022-02-12 11:01:25 -0500

    Adding last run date check to file

--- github/github.sh
+++ github/github.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 ### Mac script
 
-COUNTER=5
-until [  $COUNTER -lt 1 ]; do
-    # add a line to the test file
-    echo "adding a line" >> log.txt
-    git add .
-    git commit -m "Adding a line"
+today=$(date '+%Y-%m-%d')
+last=$(tail -n 1 log.txt)
 
-    # remove the last line from the test file
+if [ "$today" = "$last" ]
+then
+    echo "Already completed"
+else
+    echo "Running Github script"
+    COUNTER=5
+    until [  $COUNTER -lt 1 ]; do
+        # add a line to the test file
+        echo "adding a line" >> log.txt
+        git add .
+        git commit -m "Adding a line"
+
+        # remove the last line from the test file
+        sed -i '' -e '$ d' log.txt
+        git add .
+        git commit -m "Removing a line"
+
+        let COUNTER-=1
+    done
+
+    # remove the date from the test file
     sed -i '' -e '$ d' log.txt
-    git add .
-    git commit -m "Removing a line"
+    # add the last run date to the file
+    echo $today >> log.txt
 
-    let COUNTER-=1
-done
-
-git push origin master
+    git push origin master
+fi
